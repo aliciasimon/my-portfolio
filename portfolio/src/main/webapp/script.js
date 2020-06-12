a01f6c4606d387769414abd3288f478d27f7e7ac
aliciasimon <alicialeesimon@gmail.com>
2020-06-12 19:56:10 +0000

    Added Dark Mode, Satellite View, Markers, Map is centered on my hometown

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -28,10 +28,115 @@ function getJsonString(){
  */
 var map;
 function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8
+    // Creating a StyledMap object and passing an array of styles into it
+    // Displays a 'Styled Map' button at the top of the map that the user can click to view
+    var styledMapType = new google.maps.StyledMapType( // code for 'Dark Mode' from the Maps JavaScript API Styling Document
+        [
+            {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
+            {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
+            {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
+            {
+              featureType: 'administrative.locality',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#d59563'}]
+            },
+            {
+              featureType: 'poi',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#d59563'}]
+            },
+            {
+              featureType: 'poi.park',
+              elementType: 'geometry',
+              stylers: [{color: '#263c3f'}]
+            },
+            {
+              featureType: 'poi.park',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#6b9a76'}]
+            },
+            {
+              featureType: 'road',
+              elementType: 'geometry',
+              stylers: [{color: '#38414e'}]
+            },
+            {
+              featureType: 'road',
+              elementType: 'geometry.stroke',
+              stylers: [{color: '#212a37'}]
+            },
+            {
+              featureType: 'road',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#9ca5b3'}]
+            },
+            {
+              featureType: 'road.highway',
+              elementType: 'geometry',
+              stylers: [{color: '#746855'}]
+            },
+            {
+              featureType: 'road.highway',
+              elementType: 'geometry.stroke',
+              stylers: [{color: '#1f2835'}]
+            },
+            {
+              featureType: 'road.highway',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#f3d19c'}]
+            },
+            {
+              featureType: 'transit',
+              elementType: 'geometry',
+              stylers: [{color: '#2f3948'}]
+            },
+            {
+              featureType: 'transit.station',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#d59563'}]
+            },
+            {
+              featureType: 'water',
+              elementType: 'geometry',
+              stylers: [{color: '#17263c'}]
+            },
+            {
+              featureType: 'water',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#515c6d'}]
+            },
+            {
+              featureType: 'water',
+              elementType: 'labels.text.stroke',
+              stylers: [{color: '#17263c'}]
+            }
+        ],
+        {name: 'Dark Mode'});
+
+
+    var map = new google.maps.Map(document.getElementById("map"), {
+        center: {lat: 30.005417, lng: -91.818665}, 
+        zoom: 9,
+        mapTypeControlOptions: {
+            mapTypeIds: ['roadmap','satellite','styled_map']
+        }
   });
+
+  //Associate the styled map with the MapTypeId and set it to display.
+    map.mapTypes.set('styled_map', styledMapType);
+    map.setMapTypeId('styled_map');
+
+    // New Iberia Marker
+    var new_iberia = {lat: 30.005417, lng: -91.818665};
+    marker = new google.maps.Marker({position: new_iberia, map: map});
+
+    // XULA Marker 
+    var new_orleans = {lat: 29.9652, lng: -90.1070};
+    marker2 = new google.maps.Marker({position: new_orleans, map: map});
+    
+    // Lafayette Marker 
+    var lafayette = {lat: 30.2241, lng: -92.0198};
+    marker3 = new google.maps.Marker({position: lafayette, map: map});
 }
 
 /**
